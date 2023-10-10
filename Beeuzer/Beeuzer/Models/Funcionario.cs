f48079e1fde900dd2e00d460a35301d03f1fb824
SthephanySouza <101808324+SthephanySouza@users.noreply.github.com>
2023-10-10 16:31:10 -0300

    Update
    Tirando views, cadastro e login, e criando classes, cliente e funcionário.

---
+++ Beeuzer/Beeuzer/Models/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Beeuzer.Models
+{
+    public class Funcionario
+    {
+        public int IdFunc { get; set; }
+        public string NomeFunc{ get; set; }
+        public string CNPJ { get; set; }
+        public string NumEnd { get; set; }
+        public string CompleEnd { get; set; }
+        public string Telefone { get; set; }
+        public string CepFunc { get; set; }
+    }
+}
