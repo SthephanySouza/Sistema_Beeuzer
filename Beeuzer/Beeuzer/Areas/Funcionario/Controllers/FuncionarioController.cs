1b95aa2c57ff2ed5831059b8fbbd9ffe7186d65b
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2023-11-19 18:13:36 -0300

    Produto e Pedido

--- Beeuzer/Beeuzer/Areas/Funcionario/Controllers/FuncionarioController.cs
+++ Beeuzer/Beeuzer/Areas/Funcionario/Controllers/FuncionarioController.cs
@@ -4,6 +4,7 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -28,7 +29,7 @@ namespace Beeuzer.Areas.Funcionario.Controllers
             {
                 connection.Open();
 
-                using (MySqlCommand cmd = new MySqlCommand("select * from vwProduto", connection))
+                using (MySqlCommand cmd = new MySqlCommand("select * from vwProduto order by CodigoBarras", connection))
                 {
                     using (MySqlDataReader reader = cmd.ExecuteReader())
                     {
@@ -53,9 +54,95 @@ namespace Beeuzer.Areas.Funcionario.Controllers
             return View(produtoViewModels);
         }
 
+        public ActionResult SelectPed()
+        {
+            List<Pedido> pedidos = new List<Pedido>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand("select NumeroPedido, NomeCli, DataPedido, DataPrazo, TotalPedido from vwPedido", connection))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Pedido pedido = new Pedido
+                            {
+                                NumeroPedido = Convert.ToInt32(reader["NumeroPedido"]),
+                                NomeCli = reader["NomeCli"].ToString(),
+                                DataPedido = Convert.ToDateTime(reader["DataPedido"]),
+                                DataPrazo = Convert.ToDateTime(reader["DataPrazo"]),
+                                TotalPedido = Convert.ToDecimal(reader["TotalPedido"])
+                            };
+
+                            pedidos.Add(pedido);
+                        }
+                    }
+                }
+            }
+
+            return View(pedidos);
+        }
+
+        [HttpGet]
         public ActionResult CadastrarProd()
         {
+            MySqlConnection conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["conexao"].ConnectionString);
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Error", new { mensagem = e.Message });
+            }
+            finally
+            {
+                if (conexao.State == ConnectionState.Open)
+                    conexao.Close();
+            }
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarProd(ProdutoViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            Produto novoProd = new Produto
+            {
+                CodigoBarras = viewModel.CodigoBarras,
+                NomeProd = viewModel.NomeProd,
+                Cor = viewModel.Cor,
+                Tamanho = viewModel.Tamanho,
+                ValorUnitario = viewModel.ValorUnitario,
+                Qtd = viewModel.Qtd
+            };
+            novoProd.InsertProd(novoProd);
+
+            return RedirectToAction("SelectProd", "Funcionario");
+        }
+
+        public ActionResult AtualizarProd([Bind(Prefix = "id")] Int64 CodigoBarras)
+        {
+            Produto produto = new Produto();
+            var Produto = produto.SelectProd(CodigoBarras);
+            return View(Produto);
+        }
+
+        [HttpPost]
+        public ActionResult AtualizarProd(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                produto.UpdateProd(produto);
+                return RedirectToAction("SelectProd", "Funcionario");
+            }
+            return View(produto);
         }
     }
 }
---
+++ Beeuzer/Beeuzer/Models/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Beeuzer.Models
+{
+    public class Pedido
+    {
+        public int NumeroPedido { get; set; }
+
+        public string NomeCli { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public DateTime DataPrazo { get; set; }
+
+        public decimal TotalPedido { get; set; }
+    }
+}
